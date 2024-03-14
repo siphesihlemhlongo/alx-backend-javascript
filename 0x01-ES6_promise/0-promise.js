fc6845cd2dc1a9f7d7499fb6dfb52cbfae48a1a4
siphesihlemhlongo <sphesihlemhlongo07@gmail.com>
2024-03-14 15:06:18 +0000

    ES6-promise

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,3 +1,7 @@
-export default function getResponseFromAPI() {
-    return new Promise((resolve, reject) => [resolve, reject]);
-  }
+export default function getListStudents() {
+  return Promise.resolve([
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' }
+  ]);
+}

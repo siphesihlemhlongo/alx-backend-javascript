5e09cde6194b36f66b7f4e5124ae7597ef821f76
siphesihlemhlongo <sphesihlemhlongo07@gmail.com>
2024-03-14 14:15:29 +0000

    ES6-promise

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,9 +1,3 @@
-function getResponseFromAPI() {
-    return new Promise((resolve, reject) =>
-        setTimeout(() => {
-            resolve("Response from API");
-        }, 1000);
-    });
-}
-
-export default getResponseFromAPI;
+export default function getResponseFromAPI() {
+    return new Promise((resolve, reject) => [resolve, reject]);
+  }

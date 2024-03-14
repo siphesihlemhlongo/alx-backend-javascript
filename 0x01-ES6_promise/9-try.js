2c408d45fb14a4aa0c05ec624f81d771617827c6
siphesihlemhlongo <sphesihlemhlongo07@gmail.com>
2024-03-14 22:03:22 +0000

    ES6-promise

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -1,16 +1,9 @@
-function guardrail(mathFunction) {
+export default function guardrail(mathFunction) {
     const queue = [];
-
     try {
-        const result = mathFunction();
-        queue.push(result);
-    } catch (error) {
-        queue.push(error.message);
-    } finally {
-        queue.push('Guardrail was processed');
+      queue.push(mathFunction(), 'Guardrail was processed');
+    } catch (err) {
+      queue.push(`Error: ${err.message}`, 'Guardrail was processed');
     }
-
     return queue;
-}
-
-export default guardrail;
+  }

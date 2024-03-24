f944292c19576a0c5b8691dc1b64159e49b3930b
siphesihlemhlongo <sphesihlemhlongo07@gmail.com>
2024-03-24 19:23:33 +0000

    promises

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,11 +1,6 @@
-function handleResponseFromAPI(promise) {
-  return promise.then((response) => {
-    console.log('Got a response from the API');
-    return { status: 200, body: 'success' };
-  }).catch((error) => {
-    console.error('Error:', error.message);
-    return new Error();
-  });
+export default function handleResponseFromAPI(promise) {
+  return promise
+    .then(() => ({ status: 200, body: 'success' }))
+    .catch(() => new Error())
+    .finally(() => console.log('Got a response from the API'));
 }
-
-export default handleResponseFromAPI;

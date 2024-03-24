cc219b954685095710022003650d81b5acd62da8
siphesihlemhlongo <sphesihlemhlongo07@gmail.com>
2024-03-24 19:09:34 +0000

    promises

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,20 +1,14 @@
-import { uploadPhoto, createUser } from './utils.js';
+import { uploadPhoto, createUser } from './utils';
 
-async function asyncUploadUser() {
-    try {
-        const [photo, user] = await Promise.all([uploadPhoto(), createUser()]);
+export default async function asyncUploadUser() {
+  let res = {};
 
-        return {
-            photo: photo,
-            user: user
-        };
-    } catch (error) {
-        console.error('Error occurred:', error);
-        return {
-            photo: null,
-            user: null
-        };
-    }
+  try {
+    const photo = await uploadPhoto();
+    const user = await createUser();
+    res = { photo, user };
+  } catch (err) {
+    res = { photo: null, user: null };
+  }
+  return res;
 }
-
-export default asyncUploadUser;

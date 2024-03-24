b738ec33aff2244a53594cbde5ae7d1f93f98e6d
siphesihlemhlongo <sphesihlemhlongo07@gmail.com>
2024-03-24 19:30:06 +0000

    promises

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,7 +1,3 @@
-export default function getListStudents() {
-  return Promise.resolve([
-    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
-    { id: 2, firstName: 'James', location: 'Columbia' },
-    { id: 5, firstName: 'Serena', location: 'San Francisco' }
-  ]);
+export default function getResponseFromAPI() {
+  return new Promise(() => {});
 }
